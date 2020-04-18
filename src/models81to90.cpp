672a256f526c1556a518ff4f958e42f2e8f28f6d
DrRaytrace <arnaud.thernisien@gmail.com>
2020-04-18 13:32:10 -0400

    Place holder for new models.

---
+++ src/models81to90.cpp
@@ -0,0 +1,9 @@
+
+#include "models81to90.h"
+#include "constant.h"
+#include <cmath>
+#include <string>
+#include <fstream>
+#include "config.h"
+#include "scene.h"
+
---
+++ src/models81to90.h
@@ -0,0 +1,13 @@
+/** \file models81to90.h
+ * \brief Model 71 to 80
+ */
+
+#ifndef MODELS81TO90_H
+#define MODELS81TO90_H
+
+#include "CModelBase.h"
+#include <string>
+
+
+
+#endif
